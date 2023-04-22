4834854ec994dc471c803689a1d7b67f3603dc7a
MrJacob252 <84764558+MrJacob252@users.noreply.github.com>
2023-04-22 12:39:21 +0200

    Fixed the axis transformation for the y axis
    The color area plotting now should work for image of any size

--- Project_2/test/gui_beta.py
+++ Project_2/test/gui_beta.py
@@ -1,33 +1,34 @@
 """
 TODO
+-- Button to choose a picture - DONE
+-- Display picture path - DONE
 
--- Button to choose a picture
-__ Display picture path
-
--- Display colors in the picture
+-- Display colors in the picture - DONE
 -- Button to choose the color - DONE
--- Generate color pickers
+-- Generate color pickers - DONE
 
--- Button to display scatter plot of the color
+-- Button to display scatter plot of the color - DONE
 
--- Button to save pixel locations to a text file
+-- Button to save pixel locations to a text file - DONE
 
--> Choose a picture
--> Display colors of the picture in a color frame widget
--> Choose a color from a color frame widget
--> Display scatter plot of the color
+-> Choose a picture                                         - DONE
+-> Display colors of the picture in a color frame widget    - DONE
+-> Choose a color from a color frame widget                 - DONE
+-> Display scatter plot of the color                        - DONE
 
--- Fix the color frame widget -> don't know how to change its color from the inside
+-- The plotting axis transformation only work for the 128x128 image
 """
 
 import customtkinter as ctk
 import matplotlib.pyplot as plt
 from tkinter import filedialog
-import tkinter as tk
 from PIL import Image
 import numpy as np
 
 class Storage:
+    """
+    Data storage class
+    """
     def __init__(self) -> None:
         self.img = None
         self.img_arr = None
@@ -35,18 +36,22 @@ class Storage:
         self.img_colors = None
 
 class App(ctk.CTk):
+    """
+    Main GUI class
+    """
     def __init__(self, title: str, fg_color = None, **kwargs):
         super().__init__(fg_color, **kwargs)
 
+        # Data storage
         self.storage = Storage()
         
-        # self.colors = np.array([[255, 255, 0, 255], 
-        #                         [255, 0, 255, 255], 
-        #                         [0, 255, 255, 255]])
-        
+        # Color widgets
         self.color_widgets = {}
+        
+        # Radio button variable
         self.color_var = ctk.IntVar()        
         
+        # Appearance setup
         self.__set_appearance_and_theme(appearance_mode='dark')
 
         self.__set_rows_and_columns(2, 2)
@@ -107,6 +112,11 @@ class App(ctk.CTk):
         # # Color Frame
     
     def upload_img(self):
+        """
+        Lets the user choose an image file.
+        Saves the image to the storage.
+        Saves the image path to the storage.
+        """
         
         file_types = [('PNG files', '*.png'), ('All files', '*')]
         file = filedialog.askopenfilename(title='Open a file', filetypes=file_types)
@@ -125,7 +135,8 @@ class App(ctk.CTk):
         
     def extract_colors(self):
         '''
-        Extracts colors from the image.
+        Creates a numpy array of the image.
+        Extracts colors from the image array.
         '''
         
         # Check if image exists
@@ -137,8 +148,6 @@ class App(ctk.CTk):
         
         # Get the unique colors
         self.storage.img_colors = np.unique(self.storage.img_arr.copy().reshape(-1, self.storage.img_arr.shape[2]), axis=0)
-        
-        # print(self.storage.img_colors)
     
     def __set_appearance_and_theme(self, appearance_mode='system', theme='blue'):
         '''
@@ -157,14 +166,15 @@ class App(ctk.CTk):
         '''
         Sets the number of rows and columns in the self or given frame.
         '''
-        
+        # Self
         if frame == None:
             for i in range(rows):
                 self.rowconfigure(i, weight=1)
                 
             for i in range(columns):
                 self.columnconfigure(i, weight=1)
-                
+        
+        # Given frame  
         else:
             for i in range(rows):
                 frame.rowconfigure(i, weight=1)
@@ -174,7 +184,7 @@ class App(ctk.CTk):
     
     def reset_color_widgets(self):
         '''
-        Destroys all color widgets.
+        Destroys all color widgets and resets the dict.
         '''
         for i in self.color_widgets:
             self.color_widgets[i].destroy()
@@ -185,23 +195,27 @@ class App(ctk.CTk):
         """
         Creates color widgets from colors stored in self.storage.colors.
         """
+        # Initialose rows and columns
         row = 0
         column = 0
         
+        # Reset color widgets
         self.reset_color_widgets()
         
+        # Extract colors
         self.extract_colors()
         
+        # Handls if no colors are present
         if type(self.storage.img_colors) != np.ndarray:
             return
-        # if self.storage.img_colors.any() == None:
-        #     return
         
+        # Create color widget for each color
         for i, color in enumerate(self.storage.img_colors):
             # self.color_widgets.append(ColorWidget(master=self.color_fr, color=color))
             self.color_widgets[str(color)] = ColorWidget(master=self.color_fr, color=color, variable=self.color_var, value=i)
             self.color_widgets[str(color)].grid(row=row, column=column)
             
+            # Increment rows and columns
             column += 1
             if column == 3:
                 row += 1
@@ -212,27 +226,35 @@ class App(ctk.CTk):
         Plots the area of the chosen color.
         """
         
+        # Handle if no colors are present
         if len(self.color_widgets) == 0:
             return
         
-        # Get the color
+        # Get the color from radio button
         color = self.color_var.get()
         color_arr = self.storage.img_colors[color]
         
+        # Generate coordinates of the color
         active_cords = self.__generate_color_cord(color_arr)
-                 
+        
+        # Creates subplot
         fig, ax = plt.subplots()
         
+        # Configure subplot axes
         ax.set_xlim(0, len(self.storage.img_arr))
         ax.set_ylim(0, len(self.storage.img_arr[0]))
         ax.set_aspect('equal')
         
+        # Plot the coordinates of the color as a scatter plot
         plot = ax.scatter([i[0] for i in active_cords], 
                           [i[1] for i in active_cords])
+        
         plt.show()
 
     def __generate_color_cord(self, color_array):
-        
+        """
+        Returns a list of coordinates of the given color.
+        """
         coordinates = []
         
         for i in range(len(self.storage.img_arr)):
@@ -241,52 +263,74 @@ class App(ctk.CTk):
                     # Coordinates transformed for origin in the bottom left corner
                     # X axis goes from left to right
                     # Y axis goes from bottom to top
-                    y = 127 - i
+                    # y = 127 - i
+                    # height of the image - y position
+                    y = (len(self.storage.img_arr) - 1) - i
                     coordinates.append([j, y])
                     
         return coordinates
     
     def save_cords_to_txt(self):
+        '''
+        Saves list of coordinates of the chosen color to a txt file.
+        '''
         
+        # Handle if no colors are present
         if len(self.color_widgets) == 0:
             return
         
+        # Get the color from radio button
         color = self.color_var.get()
         color_arr = self.storage.img_colors[color]
         
+        # Generate coordinates of the color
         active_cords = self.__generate_color_cord(color_arr)
         
+        # Write the coordinates to a txt file
         with open('cords.txt', 'w') as f:
             for cord in active_cords:
                 
-                if cord == active_cords[-1]:
-                    f.write(f'{cord[0]} {cord[1]}')
-                
+                if cord != active_cords[-1]:
+                    f.write(f'{cord[0]} {cord[1]}\n')
+                # Last coordinate line does not have a new line character
                 else:
-                    f.write(f'{cord[0]} {cord[1]}\n')  
+                    f.write(f'{cord[0]} {cord[1]}')  
 
 
 class ColorWidget(ctk.CTkFrame):
+    '''
+    Color widget class.
+    '''
     def __init__(self, master: any, 
                  color, variable, value,
                  width: int = 200, height: int = 200, corner_radius: int | str | None = None, border_width: int | str | None = None, bg_color: str | tuple[str, str] = "transparent", fg_color: str | tuple[str, str] | None = None, border_color: str | tuple[str, str] | None = None, background_corner_colors: tuple[str | tuple[str, str]] | None = None, overwrite_preferred_drawing_method: str | None = None, **kwargs):
         super().__init__(master, width, height, corner_radius, border_width, bg_color, fg_color, border_color, background_corner_colors, overwrite_preferred_drawing_method, **kwargs)
         
-        
+        # Color format from the picture array
         self.color: np.array[int, int, int, int] = color
+        # Convert given color to hex string format
         self.hex_color: str = "#%02x%02x%02x" % (color[0], color[1], color[2])
         
+        # Calculate text color as an inverse of the given color
         self._text_color: np.array[int, int, int, int] = 255 - self.color
         self._text_hex_color: str = "#%02x%02x%02x" % (self._text_color[0], self._text_color[1], self._text_color[2])
         
+        # Set the background color of the widget
         self.configure(fg_color=self.hex_color)
         
-        # # Widgets
+        # Radio button
         self.radio_button = ctk.CTkRadioButton(self, text="R: %d\nG: %d\nB: %d" % (color[0], color[1], color[2]), text_color=self._text_hex_color, variable=variable, value=value)
         self.radio_button.pack()
-        
+    
+    # # Getters
     def get_color(self):
+        '''
+        Returns the color of the widget as np array.
+        '''
         return self.color
     
     def get_var_val(self):
+        '''
+        Returns the value of the radio button variable.
+        '''
         return self.radio_button.getint()
